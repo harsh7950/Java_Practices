3be81f80349da3fdff8ac28c77d4c40e3443f511
Harsh Upadhyay <harsh7950@gmail.comm>
2018-08-02 11:53:58 -0400

    Playing with Lambda Collection sorting

--- Java_LambdaFunc/src/Main.java
+++ Java_LambdaFunc/src/Main.java
@@ -1,19 +1,31 @@
+import java.awt.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.function.BiConsumer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int k[] = new int[] {1,4,5,6,7};
+		String v[] = new String[]{"a","k","x","p"};
+
+	      java.util.List<String> list1 = Arrays.asList(v);
+	      	Collections.sort(list1);
+
+	      // printing the list
+	      System.out.println("The list is:" + list1);
 		
-		MyLambda Addfunction =( a,b) ->  a+b;
-		System.out.println(Addfunction.add(4, 5) );
-		
+		MyLambda Addfunction = (a, b) -> a + b;
+		System.out.println(Addfunction.add(4, 5));
+
 	}
 
-interface MyLambda{
-	int add(int a , int b);
-}
-	 
+	interface MyLambda {
+		int add(int a, int b);
+	}
 	
 	
 }
+
